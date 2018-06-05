a11fe6104abc47dd4243c7cc538eb6455a650f87
maniacmao <m13968021734@163.com>
2018-06-06 00:53:40 +0800

    界面拼接版本

---
+++ pick.cli/assets/Script/login.js
@@ -0,0 +1,61 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        label_name: {
+            default: null,
+            type: cc.EditBox
+        },     
+        label_pwd: {
+            default: null,
+            type: cc.EditBox
+        },     
+        btn_login: {
+            default: null,
+            type: cc.Button
+        },           
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+    	var self = this; // 闭包变量
+        this.btn_login.node.on(cc.Node.EventType.TOUCH_END, function (event) {
+            console.log("TOUCH_END" + self)
+            console.log("TOUCH_END" + self.label_name.string)
+            console.log("TOUCH_END" + self.label_pwd.string)
+            cc.director.loadScene('scene/main');
+        });   
+
+    },
+
+    start () { 	
+    },
+
+    // update (dt) {},
+});
---
+++ pick.cli/assets/Script/main.js
@@ -0,0 +1,67 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        btn_main: {
+            default: null,
+            type: cc.Button
+        },     
+        btn_order: {
+            default: null,
+            type: cc.Button
+        },    
+        btn_favorite: {
+            default: null,
+            type: cc.Button
+        },     
+        btn_building: {
+            default: null,
+            type: cc.Button
+        },         
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+    	var self = this; // 闭包变量
+        this.btn_favorite.node.on(cc.Node.EventType.TOUCH_END, function (event) {
+            cc.director.loadScene('scene/favorite');
+        });   
+        this.btn_order.node.on(cc.Node.EventType.TOUCH_END, function (event) {
+            cc.director.loadScene('scene/order');
+        });   
+        this.btn_building.node.on(cc.Node.EventType.TOUCH_END, function (event) {
+            cc.director.loadScene('scene/building');
+        });   
+    },
+
+    start () { 	
+    },
+
+    // update (dt) {},
+});
